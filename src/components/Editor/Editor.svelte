dd0b11fe8d6bf9cdef62dfc35600a85d20aad5ac
Lelluch <avengersis@mail.ru>
2020-07-30 12:07:40 +0000

    matryoshka was added

--- src/components/Editor/Editor.svelte
+++ src/components/Editor/Editor.svelte
@@ -1,16 +1,25 @@
 <script>
+  import Red from "./red.svelte";
+  import Blue from "./blue.svelte";
+  import Green from "./green.svelte";
   export let info = {
     in: [
       {
-        lvl: 1,
-        name: "lvl",
+        obj: Red,
+        name: "Red",
         text: "Some text",
         in: [
           {
-            lvl: 2,
-            name: "lvl",
+            obj: Blue,
+            name: "Blue",
             text: "Next some text",
-            in: [{ lvl: 3, name: "lvl", text: "text" }]
+            in: [
+              {
+                obj: Green,
+                name: "Green",
+                text: "text"
+              }
+            ]
           }
         ]
       }
@@ -18,61 +27,21 @@
   };
   $: childrens = info.in;
 
-  function addHendler(children) {
-    let counter = children.lvl;
-    counter++;
-    if (children.in) {
-      children.in = [
-        ...children.in,
-        { lvl: counter, name: "lvl", text: "Some text again" }
+  function add(event) {
+    let data = event.detail;
+    if (data.in) {
+      data.in = [
+        ...data.in,
+        { obj: data.obj, name: data.name, text: "Some text again" }
       ];
-      console.log(children.in);
       childrens = childrens;
     } else {
-      children.in = [{ lvl: counter, name: "lvl", text: "Some text again" }];
-      console.log(children.in);
+      data.in = [{ obj: data.obj, name: data.name, text: "Some text again" }];
       childrens = childrens;
     }
   }
 </script>
 
-<style>
-  .conteiner {
-    width: 400px;
-    margin: 0 auto;
-    border: 2px solid black;
-  }
-  .text {
-    height: 50px;
-  }
-  .text:focus {
-    outline: none;
-  }
-  header {
-    border-bottom: 2px solid black;
-  }
-  button {
-    display: block;
-    margin: 5px auto;
-  }
-  h1 {
-    text-align: center;
-    padding-bottom: 1px;
-    margin: 0;
-  }
-</style>
-
 {#each childrens as children (children)}
-  <div class="conteiner">
-    <header>
-      <button on:click={() => addHendler(children)}>Add</button>
-      <h1>{children.name}{children.lvl}</h1>
-    </header>
-    <div class="text" contenteditable="true">{children.text}</div>
-    <div class="editor">
-      {#if children.in}
-        <svelte:self info={children} />
-      {/if}
-    </div>
-  </div>
+  <svelte:component this={children.obj} {children} on:add={add} />
 {/each}
---
+++ src/components/Editor/blue.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Editor from "./Editor.svelte";
+  import { createEventDispatcher } from "svelte";
+  export let children = {};
+
+  let color = "blue";
+  $: chil = children.in;
+
+  const dispatch = createEventDispatcher();
+
+  function addHendler(children) {
+    dispatch("add", children);
+  }
+</script>
+
+<style>
+  .conteiner {
+    width: 200px;
+    margin: 0 auto;
+  }
+  .text {
+    height: 50px;
+  }
+  .text:focus {
+    outline: none;
+  }
+
+  button {
+    display: block;
+    margin: 5px auto;
+  }
+  h1 {
+    text-align: center;
+    padding-bottom: 1px;
+    margin: 0;
+  }
+</style>
+
+<div class="conteiner" style="border: 2px solid {color};">
+  <header style="border: 2px solid {color};">
+    <button on:click={() => addHendler(children)}>Add</button>
+    <h1>{children.name}test2</h1>
+  </header>
+  <div class="text" contenteditable="true">{children.text}</div>
+  {#if chil}
+    <Editor info={children} on:add />
+  {/if}
+</div>
